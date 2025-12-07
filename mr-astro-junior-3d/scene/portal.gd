35a2a1f0e9d2e40eab1090d982cf29a1d6155c54
Milos Farkas <sapka12@users.noreply.github.com>
2025-12-07 10:53:59 +0100

    fix: update portal door references and add blinking effect - day7

--- mr-astro-junior-3d/scene/portal.gd
+++ mr-astro-junior-3d/scene/portal.gd
@@ -3,14 +3,20 @@ extends Node3D
 var open: bool = false
 
 func _ready() -> void:
-	$CSGBox3D.material.albedo_color = Color.RED
+	$PortalDoor.material.albedo_color = Color.RED
 	State.open_portal_signal.connect(open_portal)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is PlayerCharacter and open:
-		get_tree().change_scene_to_file("res://scene/level.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://scene/level.tscn")
+
 
 func open_portal():
 	open = true
-	$CSGBox3D.material.albedo_color = Color.GREEN
-	print("portal opened")
+	$PortalDoor.material.albedo_color = Color.GREEN
+
+
+
+func _on_door_blink_timer_timeout() -> void:
+	$PortalDoor.visible = not $PortalDoor.visible
+	
