7e17ba98c1e06d0653b4422497135a8970a02244
Milos Farkas <milosfarkas@users.noreply.github.com>
2026-07-04 12:44:20 +0200

    chest fine

--- mr-astro-junior-3d/scene/chest.gd
+++ mr-astro-junior-3d/scene/chest.gd
@@ -1,10 +1,35 @@
 extends Node3D
 
+const HINT_DIAMOND_SCENES: Array[String] = [
+	"res://scene/diamond_green.tscn",
+	"res://scene/diamond_blue.tscn",
+	"res://scene/diamond_yellow.tscn",
+]
+
 @export var required_item_count: int = 3
 @export var key_spawn_offset: Vector3 = Vector3(0, 0.5, 0)
 @export var key_target: NodePath
 
 var _opened: bool = false
+
+func _ready() -> void:
+	_spawn_hint_diamonds()
+
+func _spawn_hint_diamonds() -> void:
+	var spawn_points: Array[Node] = []
+	for i in range(1, HINT_DIAMOND_SCENES.size() + 1):
+		var marker = get_node_or_null("HintDiamondSpawn%d" % i)
+		if marker:
+			spawn_points.append(marker)
+	for i in range(spawn_points.size()):
+		var scene: PackedScene = load(HINT_DIAMOND_SCENES[i])
+		var diamond: Node3D = scene.instantiate()
+		add_child(diamond)
+		diamond.position = spawn_points[i].position
+		diamond.scale = Vector3(0.2, 0.2, 0.2)
+		var area = diamond.get_node_or_null("Area3D")
+		if area:
+			area.set_deferred("monitoring", false)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if _opened:
