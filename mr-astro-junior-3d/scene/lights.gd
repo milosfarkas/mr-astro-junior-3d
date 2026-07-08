633a4339f4b95897b236d57377978f5033f2a089
Arnold Farkas <sapka12@users.noreply.github.com>
2026-07-08 18:08:48 +0200

    wip

--- mr-astro-junior-3d/scene/lights.gd
+++ mr-astro-junior-3d/scene/lights.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-const ROTATION_SPEED: float = TAU / 60.0
+const ROTATION_SPEED: float = TAU / (10 * 8 * 60.0)
 
 func _process(delta: float) -> void:
 	rotate_y(ROTATION_SPEED * delta)
