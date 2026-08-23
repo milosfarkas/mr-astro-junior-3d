cf460c5fa9b1bf4e30113aa80af7b4645a279cc1
Arnold Farkas <sapka12@users.noreply.github.com>
2026-08-23 11:22:04 +0200

    upside down box

--- mr-astro-junior-3d/scene/level_6.gd
+++ mr-astro-junior-3d/scene/level_6.gd
@@ -4,16 +4,26 @@ const RAMP_SCENE: PackedScene = preload("res://scene/ramp.tscn")
 
 func _ready() -> void:
 	super._ready()
-	var box: Box = Box.create("res://scene/box_start.tscn")
-	$Boxes.add_child(box)
-	box.open_gate(false, false, false, false)
+
+	var box1: Box = Box.create("res://scene/box_start.tscn")
+	$Boxes.add_child(box1)
+	box1.open_gate(false, false, false, false)
+	box1.get_node("Walls/Ceiling").visible = true
+	box1.get_node("Walls/Ceiling/Gate").visible = true
 
 	var ceiling_ramp: Ramp = RAMP_SCENE.instantiate()
-	box.get_node("Objects").add_child(ceiling_ramp)
+	box1.get_node("Objects").add_child(ceiling_ramp)
 	ceiling_ramp.transform = Transform3D(Basis.IDENTITY.rotated(Vector3(0, 0, 1), PI), Vector3(5.39032, 9.652115, -9.44743))
 	var ceiling_faces: Array[Box.Face] = [Box.Face.TOP, Box.Face.BACK]
 	ceiling_ramp.faces = ceiling_faces
-	ceiling_ramp.box = box
+	ceiling_ramp.box = box1
+
+	var box2: Box = Box.create("res://scene/box_start.tscn")
+	$Boxes.add_child(box2)
+	box2.open_gate(false, false, false, false)
+	box2.get_node("Walls/Ceiling").visible = true
+	box2.get_node("Walls/Ceiling/Gate").visible = true
+	box2.transform = Transform3D(Basis.IDENTITY.rotated(Vector3(1, 0, 0), PI), Vector3(0, 20, 0))
 
 	var portal_scene: PackedScene = load("res://scene/portal.tscn")
 	var portal: Node3D = portal_scene.instantiate()
