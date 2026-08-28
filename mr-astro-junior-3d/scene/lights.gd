e5fed7285ff328ba1b5b41073de5995655982207
Arnold Farkas <sapka12@users.noreply.github.com>
2026-08-29 00:51:21 +0200

    wip

--- mr-astro-junior-3d/scene/lights.gd
+++ mr-astro-junior-3d/scene/lights.gd
@@ -3,5 +3,11 @@ extends Node3D
 const number_of_rounds_per_minute: float = 1
 const ROTATION_SPEED: float = number_of_rounds_per_minute * TAU / 60.0
 
+@onready var sides: Node3D = $sides
+@onready var sides2: Node3D = $sides2
+@onready var corners: Node3D = $corners
+
 func _process(delta: float) -> void:
-	rotate_y(ROTATION_SPEED * delta)
+	sides.rotate_y(ROTATION_SPEED * delta)
+	sides2.rotate_y(-ROTATION_SPEED * delta)
+	corners.rotate_y(ROTATION_SPEED * 0.5 * delta)
