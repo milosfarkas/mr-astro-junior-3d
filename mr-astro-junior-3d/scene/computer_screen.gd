4dc657443515fd7cd43ebc9fcc397d3710db6c86
Arnold Farkas <sapka12@users.noreply.github.com>
2026-08-29 17:44:42 +0200

    screen works

---
+++ mr-astro-junior-3d/scene/computer_screen.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+const VIDEO_PATH: String = "res://assets/video/mrkicsi.ogv"
+
+@onready var _viewport: SubViewport = $VideoViewport
+@onready var _video_player: VideoStreamPlayer = $VideoViewport/VideoPlayer
+@onready var _screen_mesh: MeshInstance3D = $ScreenMesh
+
+func _ready() -> void:
+	var stream: VideoStream = load(VIDEO_PATH)
+	if stream == null:
+		push_warning("Video stream not found: %s" % VIDEO_PATH)
+		return
+	_video_player.stream = stream
+	_setup_material()
+	_video_player.finished.connect(_on_video_finished)
+	_video_player.play()
+
+func _on_video_finished() -> void:
+	_video_player.play()
+
+func _setup_material() -> void:
+	var mat: StandardMaterial3D = StandardMaterial3D.new()
+	mat.albedo_texture = _viewport.get_texture()
+	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat.cull_mode = BaseMaterial3D.CULL_DISABLED
+	_screen_mesh.material_override = mat
