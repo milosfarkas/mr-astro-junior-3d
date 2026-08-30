6239881f6a1632890fa845982de440a1ad523f8c
Arnold Farkas <sapka12@users.noreply.github.com>
2026-08-30 16:46:29 +0200

    sparkles

--- mr-astro-junior-3d/scene/credits.gd
+++ mr-astro-junior-3d/scene/credits.gd
@@ -9,8 +9,10 @@ const TRACK_PATHS: Array[String] = [
 
 @onready var music: AudioStreamPlayer = $Music
 @onready var back_button: Button = $MarginContainer/VBoxContainer/BackButton
+@onready var sparkles: CPUParticles2D = $Sparkles
 
 var _last_index: int = -1
+var _vp_size: Vector2
 
 
 func _ready() -> void:
@@ -19,6 +21,15 @@ func _ready() -> void:
 	if not music.finished.is_connected(_on_music_finished):
 		music.finished.connect(_on_music_finished)
 	_play_random_track()
+	_vp_size = get_viewport().get_visible_rect().size
+	sparkles.set_as_top_level(true)
+	sparkles.emitting = true
+	sparkles.position = Vector2(randf() * _vp_size.x, randf() * _vp_size.y)
+	for i in 99:
+		var copy: CPUParticles2D = sparkles.duplicate()
+		copy.position = Vector2(randf() * _vp_size.x, randf() * _vp_size.y)
+		copy.emitting = true
+		add_child(copy)
 
 
 func _on_back() -> void:
