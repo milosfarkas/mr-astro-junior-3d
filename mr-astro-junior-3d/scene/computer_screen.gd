e9a8f154d2fafccf200af1e3f7b33d90c2fd3af2
Arnold Farkas <sapka12@users.noreply.github.com>
2026-08-30 13:49:38 +0200

    screen works only close

--- mr-astro-junior-3d/scene/computer_screen.gd
+++ mr-astro-junior-3d/scene/computer_screen.gd
@@ -2,9 +2,18 @@ extends Node3D
 
 const VIDEO_PATH: String = "res://assets/video/mrkicsi.ogv"
 
+@export var collision_size: Vector3 = Vector3(1.0, 1.0, 0.3)
+@export var stop_delay: float = 3.0
+
 @onready var _viewport: SubViewport = $VideoViewport
 @onready var _video_player: VideoStreamPlayer = $VideoViewport/VideoPlayer
 @onready var _screen_mesh: MeshInstance3D = $ScreenMesh
+@onready var _area: Area3D = $Area3D
+@onready var _collision_shape: CollisionShape3D = $Area3D/CollisionShape3D
+
+var _stop_timer: SceneTreeTimer
+var _is_playing: bool = false
+var _mat: StandardMaterial3D
 
 func _ready() -> void:
 	var stream: VideoStream = load(VIDEO_PATH)
@@ -14,15 +23,49 @@ func _ready() -> void:
 	_video_player.stream = stream
 	_setup_material()
 	_video_player.finished.connect(_on_video_finished)
-	_video_player.play()
+	_setup_collision()
+	_area.body_entered.connect(_on_body_entered)
+	_area.body_exited.connect(_on_body_exited)
 
-func _on_video_finished() -> void:
-	_video_player.play()
+func _setup_collision() -> void:
+	var shape: BoxShape3D = _collision_shape.shape
+	if shape == null:
+		shape = BoxShape3D.new()
+		_collision_shape.shape = shape
+	shape.size = collision_size
 
 func _setup_material() -> void:
-	var mat: StandardMaterial3D = StandardMaterial3D.new()
-	mat.albedo_texture = _viewport.get_texture()
-	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	mat.cull_mode = BaseMaterial3D.CULL_DISABLED
-	mat.albedo_color = Color(0.2, 0.2, 0.4, 1.0)
-	_screen_mesh.material_override = mat
+	_mat = StandardMaterial3D.new()
+	_mat.albedo_texture = _viewport.get_texture()
+	_mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	_mat.cull_mode = BaseMaterial3D.CULL_DISABLED
+	_mat.albedo_color = Color(0.0, 0.0, 0.0, 1.0)
+	_screen_mesh.material_override = _mat
+
+func _on_video_finished() -> void:
+	if _is_playing:
+		_video_player.play()
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is PlayerCharacter:
+		_stop_timer = null
+		_start_video()
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is PlayerCharacter:
+		_schedule_stop()
+
+func _start_video() -> void:
+	_is_playing = true
+	_mat.albedo_color = Color(1.0, 1.0, 1.0, 1.0)
+	if not _video_player.is_playing():
+		_video_player.play()
+
+func _schedule_stop() -> void:
+	_stop_timer = get_tree().create_timer(stop_delay)
+	_stop_timer.timeout.connect(_stop_video)
+
+func _stop_video() -> void:
+	_is_playing = false
+	_video_player.stop()
+	_mat.albedo_color = Color(0.0, 0.0, 0.0, 1.0)
